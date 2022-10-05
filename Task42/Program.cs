a5c85e0c42097460f813d8d700147a8121c71dfc
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-05 11:35:23 +0300

    задача42

---
+++ Task42/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+void SwapArrayTwo(int[] arr)
+{
+    int swap;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        swap = arr[arr.Length - i - 1];
+        arr[arr.Length - i - 1] = arr[i];
+        arr[i] = swap;
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+Console.WriteLine("Введите число десятичной системы: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int temp = a;
+int index =0;
+while(temp > 0)
+{
+    index++;
+    temp /= 2;
+}
+int[] arr = new int[index];
+for (int i = 0; i < arr.Length; i++)
+{
+   arr[i] = a % 2;
+   a /= 2;
+}
+
+SwapArrayTwo(arr);
+PrintArray(arr); 
