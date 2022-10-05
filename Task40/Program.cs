ce95589efbee66e6806835a1177b1c54860dd908
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-05 17:17:58 +0300

    задача40правки

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -1,38 +1,18 @@
 ﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может
 // ли существовать треугольник с сторонами такой длины.
 
-void FillArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.WriteLine("Введите число: ");
-        arr[i] = Convert.ToInt32(Console.ReadLine());
-    }
-}
+Console.WriteLine("Введите сторону А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C: ");
+int c = Convert.ToInt32(Console.ReadLine());
 
-void CheckArray(int[] arr)
+if (a+b>c && a+c>b && b+c>a)
 {
-    int max = arr[0];
-    int summ = 0;
-    int indMax = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i]>max)
-        {
-            max = arr[i];
-            indMax = i;
-            summ = summ + arr[i];
-        }
-    }
-    summ = summ - arr[indMax];
-    if (arr[indMax] < summ)
-    {
-        Console.WriteLine ("Этот треугольник существует!");
-    }
-    else 
-    Console.WriteLine ("Этот треугольник не может существовать!");
+    Console.WriteLine("Да, с такими сторонами треугольник существует!");
 }
-
-int[] array = new int [3];
-FillArray(array);
-CheckArray(array);
+else
+{
+    Console.WriteLine("Нет, с такими сторонами треугольника не существует!");
+}
